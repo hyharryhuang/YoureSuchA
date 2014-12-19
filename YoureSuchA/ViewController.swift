b143b177c875306abc993401e625e0ee57494365
hhuang <hy.harry.huang@gmail.com>
2014-12-19 19:03:29 +0000

    Video instructions

--- YoureSuchA/ViewController.swift
+++ YoureSuchA/ViewController.swift
@@ -7,17 +7,74 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
 
+    let iPhone4Height:CGFloat = 480.0
+    let kOriginalPhoneScreenHeight:CGFloat = 702 * 0.6;
+    let kOriginalPhoneScreenWidth:CGFloat = 396 * 0.6;
+    
+    var moviePlayer:MPMoviePlayerController!
+    var shouldShowMovie:Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "setupMovie", name: UIApplicationDidBecomeActiveNotification, object: nil)
+        setupMoviePlayer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    var frameAdded = false
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    func setupMoviePlayer()
+    {
+        var videoURL:NSURL = NSBundle.mainBundle().URLForResource("instructions", withExtension: "mov")!
+        
+        self.moviePlayer = MPMoviePlayerController(contentURL: videoURL)
+        self.moviePlayer.controlStyle = MPMovieControlStyle.None
+        self.moviePlayer.scalingMode = MPMovieScalingMode.AspectFit
+        self.view.addSubview(self.moviePlayer.view)
+        self.moviePlayer.shouldAutoplay = true
+        self.moviePlayer.repeatMode = MPMovieRepeatMode.One
+    }
+    
+    func setupMovie()
+    {
+        var phoneScalingFactor:CGFloat = UIScreen.mainScreen().applicationFrame.size.height/self.iPhone4Height
+        
+        var x:CGFloat = (self.view.bounds.size.width - self.kOriginalPhoneScreenWidth * phoneScalingFactor)/2
+        var y:CGFloat = (self.view.bounds.size.height - self.kOriginalPhoneScreenHeight * phoneScalingFactor)/2
+
+        self.moviePlayer.view.frame = CGRect(x: x, y: y, width: self.kOriginalPhoneScreenWidth * phoneScalingFactor, height: self.kOriginalPhoneScreenHeight * phoneScalingFactor)
+        
+        self.moviePlayer.play()
+    }
+    
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        shouldShowMovie = false
+        self.moviePlayer.stop()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        shouldShowMovie = true
+        self.setupMovie()
     }
 
     
